de128bc92f443a72f53ab77686ff5c0b70fb00c7
Shivanshhh <65827695+Shivanshhh@users.noreply.github.com>
2021-08-14 22:10:27 +0530

    db.js update
    .

--- models/db.js
+++ models/db.js
@@ -1,11 +1,4 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const MONGO_URL="mongodb+srv://strtiwari28:qwerty1@cluster0-8ac9j.mongodb.net/events?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
-mongoose.connect(process.env.MONGO_URL, (err) => {
-    if (err) {
-        console.log(`Error: ${err}`);
-    } else {
-        console.log('Mongo Connection Success');
-    }
-});
+mongoose.connect('mongodb+srv://strtiwari28:qwerty1@cluster0-8ac9j.mongodb.net/events?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true',{useNewUrlParser: true});
 
